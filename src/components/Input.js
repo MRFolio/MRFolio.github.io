51c8ef34b4e59ce7179feebe30a6e95f546e7f50
MRFolio <roose.markus@gmail.com>
2021-03-16 14:29:32 +0200

    feat(geolocation): add user geolocation functionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,30 +1,66 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const API_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const Input = () => {
+  const [currentLocation, setCurrentLocation] = useState({});
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   const fetchCurrentCityWeather = async (city) => {
+  //     const url = `${API_BASE_URL}?&q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+  //     setLoading(true);
+
+  //     try {
+  //       const response = await fetch(url);
+  //       const data = await response.json();
+  //       if (data) {
+  //       } else {
+  //         setError('Something went wrong. Nothing to show.');
+  //       }
+  //     } catch (error) {
+  //       setError(error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchCurrentCityWeather('tartu');
+  // }, [query]);
+
   useEffect(() => {
-    const fetchCurrentCityWeather = async (city) => {
-      const url = `${API_BASE_URL}?&q=${city}&appid=${process.env.REACT_APP_GOOGLE_KEY}&units=metric`;
-      setLoading(true);
+    if (navigator.geolocation && currentLocation.lat && currentLocation.lon) {
+      const getWeatherForecast = async (latitude, longitude) => {
+        setLoading(true);
+        try {
+          const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+          const response = await fetch(url);
+          const data = await response.json();
+          console.log(data);
 
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        if (data) {
-        } else {
-          setError('Something went wrong. Nothing to show.');
+          return data;
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
         }
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+      };
+      getWeatherForecast(currentLocation.lat, currentLocation.lon);
+    }
+  }, [currentLocation]);
 
-    fetchCurrentCityWeather('tartu');
-  }, [query]);
-  return;
+  const handleClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      });
+    }
+  };
+
+  return <button onClick={handleClick}>Tere</button>;
 };
 
 export default Input;
